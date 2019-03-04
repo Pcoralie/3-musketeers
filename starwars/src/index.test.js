a9399eed0a89429516f12f624f30b779c2889abb
Pcoralie <Pcoralie@users.noreply.github.com>
2019-03-04 12:04:40 +0100

    test(index) : unit tests completed

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,25 +7,29 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(Array.isArray(['value'])).toBe(true)
+      expect(typeof 'value').toBe('string')
+
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      //expect(starWarsNames).toContain('Luke Skywalker')
+      expect.stringContaining('Luke Skywalker')
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect.not.stringContaining('Ben Quadinaros')
+      //expect(starWars.all).not.toContain('Ben Quadinaros')
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(typeof(starWars.random())).toBe('string')
     });
 
     test('should return an array of random items if passed a number', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      expect(Array.isArray(starWars.random(6))).toBe(true)
     });
   });
 });
